86f909704e59d74a9b20bcde32642dbb61c2b315
MarceloCasanova <marcelo.casanova@fatec.sp.gov.br>
2018-02-04 20:12:08 -0200

    Versão1.0.1

---
+++ CrudAspNetMvc/Controllers/testeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudAspNetMvc.Controllers
+{
+    public class testeController : Controller
+    {
+        // GET: teste
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
